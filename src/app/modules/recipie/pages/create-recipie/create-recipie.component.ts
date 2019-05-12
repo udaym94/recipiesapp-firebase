1d3ba75d9ba46a914d3d5df2cdb3e1e760dd5108
udaym94 <36027936+udaym94@users.noreply.github.com>
2019-05-12 20:21:45 +0530

    working on create recipe user end

--- src/app/modules/recipie/pages/create-recipie/create-recipie.component.ts
+++ src/app/modules/recipie/pages/create-recipie/create-recipie.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-create-recipie',
@@ -8,15 +10,19 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class CreateRecipieComponent implements OnInit {
   addRecipeForm: FormGroup;
+  image: File = null;
+  imageDesination = 'recipes-cover/';
+  imageURL: Observable<string | null>;
+
   constructor(
     private formBuilder: FormBuilder,
+    private firebaseStorage: AngularFireStorage
   ) { }
 
   ngOnInit() {
     this.addRecipeForm = this.formBuilder.group({
       'title': ['', Validators.required],
-      'description': ['', Validators.required],
-      'image': ['', Validators.required],
+      'description': ['', Validators.required]
     });
   }
 
@@ -30,8 +36,27 @@ export class CreateRecipieComponent implements OnInit {
     return this.addRecipeForm.get('description');
   }
 
-  get image() {
-    return this.addRecipeForm.get('image');
+  // get image() {
+  //   return this.addRecipeForm.get('image');
+  // }
+
+  async changeImage(event) {
+    this.image = event.target.files[0];
+    console.log(this.image);
+  }
+
+  async storeRecipe (addRecipeForm) {
+    const fd = new FormData();
+    if (this.image !== null) {
+      fd.append('image', this.image, this.image.name);
+    }
+    const fileLoactionWithName = `${this.imageDesination}/${this.image.name}`;
+    const imageRef = this.firebaseStorage.ref(fileLoactionWithName);
+    const uploadFile = await this.firebaseStorage.upload(fileLoactionWithName, this.image);
+    this.imageURL = imageRef.getDownloadURL();
+    console.log(uploadFile, this.imageURL);
+    fd.append('title', addRecipeForm.value.title);
+    fd.append('description', addRecipeForm.value.description);
   }
 
 }
